2bd97d6f2eced267d77251182dab296e7f56345f
Rômulo Nunes <romulotg21@gmail.com>
2024-07-11 18:59:18 -0300

    [learn] querry params

--- httpServer/src/controllers/userController.js
+++ httpServer/src/controllers/userController.js
@@ -4,8 +4,18 @@ const mocks = require("../mocks/lists.js");
 module.exports = 
 {
     getUsers: (req, res) => {
+        const {order} = req.query
+        const sortedUsers = mocks.users.sort((a, b) => {
+            if (order === "desc") {
+                return a.id < b.id ? 1 : -1
+            }
+
+            return a.id > b.id ? 1 : -1
+        })
+
         res.writeHead(200, {"Content-Type": "application/json"});
-        res.end(JSON.stringify(mocks.users));
+        res.end(JSON.stringify(sortedUsers));
+        
     },
     getProducts: (req, res) => {
         res.writeHead(200, {"Content-Type": "application/json"});
--- httpServer/src/index.js
+++ httpServer/src/index.js
@@ -1,13 +1,16 @@
 const http = require("http");
-port = 3000;
 const routes = require("./routes")
+const {URL} = require("url")
+port = 3000;
 
 const server = http.createServer((request, repsonse) => {
     console.log(`Request method:" ${request.method}`);
+    const parsedUrl = new URL("http://localhost:3000" + request.url);
+    console.log(parsedUrl)
+    const route = routes.find(route => route.endpoint === parsedUrl.pathname && route.method === request.method);
 
-    const route = routes.find(route => route.endpoint === request.url && route.method === request.method);
-    
     if (route) {
+        request.query = Object.fromEntries(parsedUrl.searchParams);
         route.handler(request, repsonse);
     } else {
         repsonse.writeHead(404, {"Content-Type": "text/plain"});
