e9661d0063fcda58386b01aec9713734941822cd
Rômulo Nunes <romulotg21@gmail.com>
2024-07-15 11:40:43 -0300

    [learn] editorconfig

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,9 +1,9 @@
-const express = require("express")
+const express = require("express");
 const port = 3003
 const app = express();
 
 app.get("/", (request, response)=>{
-    response.send("Hello world")
+    response.send("Hello world");
 })
 
 app.listen(port, ()=> console.log(`🚀 Server running at: http://localhost:${port}`))
