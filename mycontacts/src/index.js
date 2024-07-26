5561e4db082c0cbae215a531146f0893a6d5a25d
Rômulo Nunes <romulotg21@gmail.com>
2024-07-26 19:09:52 -0300

    chore: express-async-errors

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const port = 3003
 const app = express();
-const routes = require("./routes")
+const routes = require("./routes");
+require("express-async-errors")
+
 
 app.use(express.json())
 app.use(routes)
-
+app.use((error, request, response, next) =>{
+  console.log("########### Error ###########")
+  console.log(error)
+  response.sendStatus(500)
+})
 
 app.listen(port, ()=> console.log(`🚀 Server running at: http://localhost:${port} 🚀`))
