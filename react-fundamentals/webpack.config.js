d6cf3026493db9d7f16f420610199accaec9588f
Rômulo Nunes <romulotg21@gmail.com>
2024-07-30 20:12:42 -0300

    chore: html-webpack-plugin

--- react-fundamentals/webpack.config.js
+++ react-fundamentals/webpack.config.js
@@ -1,10 +1,17 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   entry: path.resolve(__dirname, "build", "index.js"),
   output: {
     path: path.resolve(__dirname + "/build"),
-    filename: "bundle[hash].js",
+    filename: "bundle[fullhash].js",
   },
   mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src","public", "index.html"),
+    }),
+  ]
+
 }
