4649aef81784ec63aad7cf20ba10f6b424614722
Rômulo Nunes <romulotg21@gmail.com>
2024-07-30 20:16:09 -0300

    chore: CleanWebpackPlugin

--- react-fundamentals/webpack.config.js
+++ react-fundamentals/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
   entry: path.resolve(__dirname, "build", "index.js"),
@@ -12,6 +13,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src","public", "index.html"),
     }),
+    new CleanWebpackPlugin(),
   ]
 
 }
