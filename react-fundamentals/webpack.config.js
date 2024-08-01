fa3f581184712e2ccacb9abcb7caa15f9fa50b2c
Rômulo Nunes <romulotg21@gmail.com>
2024-08-01 11:18:04 -0300

    chore: babel loader

--- react-fundamentals/webpack.config.js
+++ react-fundamentals/webpack.config.js
@@ -3,15 +3,24 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
-  entry: path.resolve(__dirname, "build", "index.js"),
+  entry: path.resolve(__dirname, "src", "index.js"),
   output: {
     path: path.resolve(__dirname + "/build"),
     filename: "bundle[fullhash].js",
   },
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src","public", "index.html"),
+      template: path.resolve(__dirname, "public", "index.html"),
     }),
     new CleanWebpackPlugin(),
   ]
