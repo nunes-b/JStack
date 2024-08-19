19b1421f1535bcedaf8bd02a9c513b1b2f466ac1
Rômulo Nunes <romulotg21@gmail.com>
2024-08-19 13:09:17 -0300

    learn: propTypes

--- react-fundamentals/src/App.js
+++ react-fundamentals/src/App.js
@@ -2,21 +2,34 @@ import React from "react";
 import Post from "./Post";
 import Header from "./Header";
 const category = "Posts da semana";
+
 const post = {
   title: "Primeiro post",
   subtitle: "Subtítulo do primeiro post",
   content: "Conteúdo bla bla bla do primeiro post",
   date: "01/01/2021",
+  likes: 4
 }
+
 function App() {
   return (
     <React.Fragment>
+
       <Header title = "Meu blog" >
         <h2>{category}</h2>
       </Header>
+
       <hr />
-    <Post title={post.title} subtitle={post.subtitle} content={post.content} date={post.date} />
-    <Post />
+
+    <Post
+    title={post.title}
+    subtitle={post.subtitle}
+    content={post.content}
+    date={post.date}
+    likes = {post.likes}
+     />
+
+
     </React.Fragment>
   );
 }
--- react-fundamentals/src/Post.js
+++ react-fundamentals/src/Post.js
@@ -1,5 +1,6 @@
 
 import React from "react";
+import propTypes from "prop-types";
 
 export default function Post(props) {
   return (
@@ -8,9 +9,18 @@ export default function Post(props) {
       <strong>{props.title}</strong><br />
       <small>{props.subtitle}</small><br />
       <p>{props.content}</p><br />
-      <small>{props.date}</small>
+      <small>{props.date}</small><br/>
+      <hr />
+      <small> likes: {props.likes}</small>
+      <small> média: {props.likes / 2}</small>
+      <small> viwers: {props.likes * 2}</small>
       </article>
       <br />
     </React.Fragment>
 );
+}
+
+Post.propTypes ={
+  likes: propTypes.number.isRequired,
+  title: propTypes.string.isRequired
 }
